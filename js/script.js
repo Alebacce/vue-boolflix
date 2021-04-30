5a315294623c5fa380766b00a59534483a5e15bf
Alebacce <73042051+Alebacce@users.noreply.github.com>
2021-05-01 00:39:52 +0200

    Milestone 4 completed

--- js/script.js
+++ js/script.js
@@ -6,18 +6,21 @@ var app = new Vue ({
         // searchResults is an array which contains the search results
         // with all of their data
         searchResults: [],
-        
     },
 
     methods: {
         // searchShow() call TheAovieDB API and populates searchResults array
-        searchShow() {
+        searchShow(show) {
             axios
             .get(`https://api.themoviedb.org/3/search/multi?api_key=5401b044329392c3526c0bd5381b8550&language=it-IT&query=${this.userSearch}}&page=1`)
             .then((response) => {
                 const result = response.data.results;
                 this.searchResults = result;
+                // console.log(this.searchResults[index].id);
             })
+
+            // axios
+            //     .get(`https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=5401b044329392c3526c0bd5381b8550&language=it-IT`)
         },
 
         // voteInStars() transfrom the original vote from 1 to 10 to 1 to 5
@@ -25,7 +28,6 @@ var app = new Vue ({
         voteInStars(vote) {
             const halfVote = vote / 2;
             let voteIn5 = this.roundUp(halfVote, 0);
-            console.log(voteIn5);
             return voteIn5;
         },
 
